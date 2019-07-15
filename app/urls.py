774aaa770124ba2eef6644fca988cb6345c77f33
Aleksandra Konovalova <koalasaxen@gmail.com>
2019-07-16 00:14:32 +0300

    draft commit - htmls

--- app/urls.py
+++ app/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from . import views as user_views
 
 urlpatterns = [
-    path('',user_views.home,name='home'),
+    path('',user_views.home,name='index'),
     path('register/', user_views.register,name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
