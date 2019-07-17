07d5d79451edf3f6936c2a32234c243fc7f84691
Aleksandra Konovalova <koalasaxen@gmail.com>
2019-07-17 14:54:07 +0300

    TMS-5 draft commit - register

--- app/views.py
+++ app/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from .models import Project
+from .forms import UserRegisterForm
 
-from django.shortcuts import render
 
 def home(request):
     projects = Project.objects.all()
@@ -10,4 +11,13 @@ def home(request):
     return render(request, 'index.html', context)
 
 def register(request):
-    return render(request,'register.html')
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, 'Account created for {username}!')
+            return redirect('home')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'register.html', {'form': form})
